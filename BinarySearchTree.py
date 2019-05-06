d500c486889fb44c5a3f179738f8d98b278f0390
FWSU <yys9905@naver.com>
2019-05-06 13:59:09 +0900

    Update BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -67,16 +67,86 @@ class Binary_search_tree:
 
   def print_tree(self):
     self.print_node(self.root, 0)
+
   def print_node(self,node, depth):
     if node is not None:
       print("  "*depth + str(node.get_key()))
 
       self.print_node(node.get_left(), depth+1)
       self.print_node(node.get_right(), depth+1)
+        
+  def get_parent(self, key):
+    return self.get_parent_node(self.root, key)
 
+  def get_parent_node(self, node, key):
+    if node is None:
+      return None
+    
+    if node.get_left().get_key() == key:
+      return node, 0
+    elif node.get_right().get_key() == key:
+      return node, 1
 
+    elif node.get_key() > key:
+      return self.get_parent_node(node.get_left(), key)
+    else:
+      return self.get_parent_node(node.get_right(), key)
 
-      
+  def delete(self, key):
+    parent, locate = self.get_parent(key)
+    if locate == 0:
+      node = parent.get_left()
+    elif locate == 1:
+      node = parent.get_right()
+
+    self.delete_node(parent, node, locate)
+
+  def delete_node(self, parent, node, locate):
+    if node.get_left() and node.get_right():
+      self.twoChildDelete(parent, node, locate)
+    elif node.get_left() is None and node.get_right() is None:
+      self.noneChildDelete(parent,node, locate)
+    else:
+      self.oneChildDelete(parent, node, locate)
+
+  def in_order(self, parent):
+    node = parent.get_left()
+    child = node.get_left()
+    if child is not None:
+      return self.in_order(node)
+    else:
+      return parent, node
+
+  def twoChildDelete(self, parent, node, locate):
+    leftSubtree = node.get_left()
+    rightSubtree = node.get_right()
+    orderResultParent, orderResult = self.in_order(node.get_right())
+    if locate == 0:
+      parent.set_left(orderResult)
+    else:
+      parent.set_right(orderResult)
+    orderResultParent.set_left(None)
+    orderResult.set_left(leftSubtree)
+    orderResult.set_right(rightSubtree)
+
+  def oneChildDelete(self, parent, node, locate):
+    if node.get_left():
+      if locate == 0:
+        parent.set_left(node.get_left())
+      else:
+        parent.set_left(node.get_right())
+    else:
+      if locate == 0:
+        parent.set_right(node.get_left())
+      else:
+        parent.set_right(node.get_right())
+
+  def noneChildDelete(self, parent, node, locate):
+      if locate == 0:
+        parent.set_left(None)
+      else:
+        parent.set_right(None)
+
 bst = Binary_search_tree()
 node_40 = Node(40)
 bst.insert(node_40)
@@ -108,7 +178,8 @@ node_83 = Node(83)
 bst.insert(node_83)
 node_90 = Node(90)
 bst.insert(node_90)
-
+node_99 = Node(99)
+bst.insert(node_99)
 result = bst.search(66)
 if result is None:
   print("fail")
@@ -120,5 +191,9 @@ if result is None:
   print("fail")
 else:
   print("success")
+
+bst.delete(15)
+bst.delete(66)
+bst.delete(90)
 
 bst.print_tree()
